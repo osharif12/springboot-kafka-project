2ed56e45c9d559bea6c3b68518611ffaf7116716
osharif12 <sharifo21@yahoo.com>
2020-06-14 19:07:26 -0700

    initial commit

---
+++ src/main/java/com/spring/kafka/springbootkafkaproject/SpringbootKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.springbootkafkaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/kafka/springbootkafkaproject/SpringbootKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.kafka.springbootkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
